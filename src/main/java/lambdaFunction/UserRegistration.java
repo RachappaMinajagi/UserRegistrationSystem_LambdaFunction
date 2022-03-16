fcaa9d62f11cf6f16c7f0f453895b253234d245e
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-03-16 19:48:16 +0530

    Added: Email

--- src/main/java/lambdaFunction/UserRegistration.java
+++ src/main/java/lambdaFunction/UserRegistration.java
@@ -1,20 +1,33 @@
 package lambdaFunction;
 
 /*
- * As a User need to enter a valid First Name
- * First name starts with Cap and has minimum 3 characters
- */        
+ * UC4- As a User need to follow pre-defined Mobile Format  
+ * -E.g. 91 9919819801 - Country code follow by space and 10 digit number.
+ */
 public class UserRegistration {
 	public boolean checkFName(String fName) {
 		return (fName.matches("[A-Z][a-z]{3,}"));
 
 	}
-	
+
 	/*
-	 *   As a User need to enter a valid lirstName
-	 *    firstName starts with Cap and has minimum 3 characters
+	 * As a User need to enter a valid lirstName firstName starts with Cap and has
+	 * minimum 3 characters
 	 */
 	public boolean Lname(String Lname) {
 		return (Lname.matches("[A-Z][a-z]{3,}"));
+	}
+
+	/*
+	 * Create method for phone number given format is Valid or Not to passing
+	 * parameter
+	 */
+	public boolean checkPhoneNum(String phoneNum) {
+		return (phoneNum.matches("91\\s[0-9]{10}"));
+	}
+
+	public boolean checkPhoneNum1(String phoneNum) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
--- src/test/java/UserRegTest.java
+++ src/test/java/UserRegTest.java
@@ -3,15 +3,9 @@ import org.junit.Test;
 
 import lambdaFunction.UserRegistration;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-/*
- * 
 /**
- * UC3- As a User need to enter a valid email 
- * -E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl& co)
- *
+ * UC4- As a User need to enter a valid phonenumber
+ * Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number
  */
 public class UserRegTest {
 
@@ -70,5 +64,28 @@ public class UserRegTest {
 
 	public boolean checkEmail(String emailID) {
 		return (emailID.matches("^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+([.][a-zA-Z]{2,4})*$"));
+	}
+
+        /*
+	 * Created method for Phonenum and its type whether Phonenum is valid or not
+	 */
+public void testPhoneNumber_MustReturnTrue() {
+		UserRegistration userRegistration = new UserRegistration();
+		/**
+		 * calling checkPhoneNum method to see phoneNumber is valid
+		 */
+		boolean isPhoneNumberValid = userRegistration.checkPhoneNum("91 7903990740");
+		Assert.assertTrue(isPhoneNumberValid);
+	}
+
+	@Test
+	/**
+	 * created testPhoneNumber_MustReturnFalse() for False condition
+	 */
+	public void testPhoneNumber_MustReturnFalse() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isPhoneNumberInValid = userRegistration.checkPhoneNum("7903990740");
+		Assert.assertFalse(isPhoneNumberInValid);
+	
 	}
 }
